67f9d279427fcff70de70139f818017b51041c3d
Jimmy Dansbo <jimmy@dansbo.dk>
2024-06-16 08:33:52 +0200

    Initial commit

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+rm -rf *.PRG
+acme -f cbm -o MASTER.PRG mastermind.asm
+
---
+++ mastermind.asm
@@ -0,0 +1,7 @@
+!cpu w65c02
+!src "../cx16stuff/cx16.inc"
++SYS_LINE main
+!src "vtuilib-acme.inc"
+
+main:
+	rts

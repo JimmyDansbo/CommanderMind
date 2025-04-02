5f8fd1b12e1df2e966ba5e886138be92743e97e6
Jimmy Dansbo <jimmy@dansbo.dk>
2025-04-02 07:19:30 +0200

    Fixed naming convention in build scripts

--- Makefile
+++ Makefile
@@ -2,4 +2,4 @@ all:
 	ca65 -t cx16 -o comminda.o commandermind.asm
 	cl65 -t cx16 -m commandermind.map -Ln commandermind.sym -o COMMIND.PRG commandermind.c comminda.o
 clean:
-	rm -rf COMMIND.PRG comminda.o commandermind.map commandermind.sym
+	rm -rf COMMIND.PRG commandermind.o commandermind.map commandermind.sym
--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-rm -rf COMMIND.PRG mastera.o
+rm -rf COMMIND.PRG commandermind.o
 
 #Build assembly helper-functions
-ca65 -t cx16 -o comminda.o commandermind.asm
+ca65 -t cx16 -o commandermind.o commandermind.asm
 #Build and link main program
 cl65 -t cx16 \
      -m commandermind.map -Ln commandermind.sym -o COMMIND.PRG \

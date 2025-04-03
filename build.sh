14b0ed4a2ec54c9c9401a63d5ca06765e1e0d8fe
Jimmy Dansbo <jimmy@dansbo.dk>
2025-04-03 12:35:52 +0200

    Changed build scripts again to ensure correct build and cleanup

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-rm -rf COMMIND.PRG commandermind.o
+rm -rf COMMIND.PRG comminda.o
 
 #Build assembly helper-functions
-ca65 -t cx16 -o commandermind.o commandermind.asm
+ca65 -t cx16 -o comminda.o commandermind.asm
 #Build and link main program
 cl65 -t cx16 \
      -m commandermind.map -Ln commandermind.sym -o COMMIND.PRG \
